e3fa729f181b1f9a5f6f8d1b8364bf3bcc6a6d96
Mark Brighton <marik.shevchenko@icloud.com>
2021-02-23 19:15:10 +0200

    Commit 0.0

--- Program.cs
+++ Program.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Queue__Deck
+namespace Queue__Deque
 {
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
