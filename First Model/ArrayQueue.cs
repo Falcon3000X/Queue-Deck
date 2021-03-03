9d932a0b1573c627e3ffeff1ba6250434bc19883
Mark Brighton <marik.shevchenko@icloud.com>
2021-03-03 19:17:12 +0200

    Add ArrayQueue

---
+++ First Model/ArrayQueue.cs
@@ -0,0 +1,20 @@
+﻿namespace Queue__Deck.First_Model
+{
+    public class ArrayQueue<T>
+    {
+
+        private T[] items;
+        private T Head => items[Count - 1];
+        private T Tail => items[0];
+        public int Count { get; private set; }
+
+        public ArrayQueue(int size)
+        {
+            items = new T[size]; // Установка размера массива
+        }
+
+
+
+
+    }
+}
